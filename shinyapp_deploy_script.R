bac1ae752d8589c2842f592654bf59eabe3c315b
Mikko Lähdeaho <mikko.lahdeaho@nbl.fi>
2015-06-16 23:50:22 +0300

    commit

--- shinyapp_deploy_script.R
+++ shinyapp_deploy_script.R
@@ -2,8 +2,8 @@
 install.packages('devtools')
 devtools::install_github('rstudio/shinyapps')
 shinyapps::setAccountInfo(name='lahdeaho',
-                          token='B66C0A18597B8A57E5E66FEB1642B79F',
-                          secret='Tey6XnbPpuamvqXzhfHaHjEoeRBO87mKudf99PT+')
+                          token='***',
+                          secret='***')
 
 #deploy
 library(shinyapps)
